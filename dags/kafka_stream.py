f0d5558a9907ac42e9745b6c83dfb24e59f57daa
SadafAsad <34832185+SadafAsad@users.noreply.github.com>
2024-01-21 15:34:42 -0500

    dag arg

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -1,5 +1,16 @@
 from datetime import datetime
+from datetime import timedelta
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.utils.dates import days_ago
 
-
+#DAG arguments block
+default_args = {
+    'owner': 'Sadaf Asadollahi',
+    'start_date': days_ago(0),
+    'email': ['sadaf98x@gmail.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
+}
