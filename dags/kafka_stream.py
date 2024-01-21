fd0d2313f68a62cca1ad4a26a7ae95f73a6e4a3e
SadafAsad <34832185+SadafAsad@users.noreply.github.com>
2024-01-21 15:46:18 -0500

    stream_data function works

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -1,37 +1,39 @@
 from datetime import datetime
 from datetime import timedelta
-from airflow import DAG
-from airflow.operators.python import PythonOperator
-from airflow.utils.dates import days_ago
+# from airflow import DAG
+# from airflow.operators.python import PythonOperator
+# from airflow.utils.dates import days_ago
 
-#DAG arguments block
-default_args = {
-    'owner': 'Sadaf Asadollahi',
-    'start_date': days_ago(0),
-    'email': ['sadaf98x@gmail.com'],
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5)
-}
+# #DAG arguments block
+# default_args = {
+#     'owner': 'Sadaf Asadollahi',
+#     'start_date': days_ago(0),
+#     'email': ['sadaf98x@gmail.com'],
+#     'email_on_failure': False,
+#     'email_on_retry': False,
+#     'retries': 1,
+#     'retry_delay': timedelta(minutes=5)
+# }
 
-#DAG definition block
-dag = DAG(
-    'user_automation',
-    default_args=default_args,
-    description='User Automation',
-    schedule_interval=timedelta(days=1)
-)
+# #DAG definition block
+# dag = DAG(
+#     'user_automation',
+#     default_args=default_args,
+#     description='User Automation',
+#     schedule_interval=timedelta(days=1)
+# )
 
-#Task definition block
-streaming_data = PythonOperator(
-    task_id='stream_data_from_api',
-    python_callable=stream_data
-)
+# #Task definition block
+# streaming_data = PythonOperator(
+#     task_id='stream_data_from_api',
+#     python_callable=stream_data
+# )
 
 def stream_data():
     import json
     import requests
 
     res = requests.get("https://randomuser.me/api/")
-    res.json()
+    return res.json()
+
+print(stream_data())
