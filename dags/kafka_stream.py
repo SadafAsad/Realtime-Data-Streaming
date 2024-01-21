a01a67b3d2e690d428d80fbc092e12cd4f00a0ec
SadafAsad <34832185+SadafAsad@users.noreply.github.com>
2024-01-21 15:37:43 -0500

    dag definition

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -14,3 +14,12 @@ default_args = {
     'retries': 1,
     'retry_delay': timedelta(minutes=5)
 }
+
+#DAG definition block
+dag = DAG(
+    'user_automation',
+    default_args=default_args,
+    description='User Automation',
+    schedule_interval=timedelta(days=1)
+)
+
